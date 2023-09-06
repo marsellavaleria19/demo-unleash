090006a6dc3644d02096205d55b984a2324705e6
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-09-06 13:51:41 +0700

    feat : add unleash component

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <UnleashHeader/>
+    <div class="container mx-auto">
+      <Nuxt/>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout'
+}
+</script>
