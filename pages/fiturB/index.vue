d0c2b327e9316d0e72efe77ae970b34d38421b03
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-09-06 14:36:00 +0700

    refactor : integration unleash on component

--- pages/fiturB/index.vue
+++ pages/fiturB/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <h1>FiturB</h1>
+  <h1 v-if="toggle">FiturB</h1>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
   layout: 'default',
+  data() {
+    return {
+      toggle: this.$unleash.isEnabled('NEW-FEATURE')
+     }
+  }
 }
 </script>
